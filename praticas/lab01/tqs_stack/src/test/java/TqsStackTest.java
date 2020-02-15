8775f2417b4b46864eccf5adc48bc2d1ab698d17
Vasco Ramos <vascoalramos@outlook.pt>
2020-02-15 15:35:23 +0000

    :white_check_mark: Added more tests and improving some old ones

--- praticas/lab01/tqs_stack/src/test/java/TqsStackTest.java
+++ praticas/lab01/tqs_stack/src/test/java/TqsStackTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,82 +9,123 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TqsStackTest {
 
-    private static TqsStack stack = new TqsStack();
+    private static TqsStack<Integer> testStack;
 
     @BeforeAll
-    public static void init() {
-        for(int i = 0; i < 4; i++) {
-            stack.push((i+1)*4);
+    static void setup() {
+        testStack = new TqsStack<>();
+        for (int i = 0; i < 4; i++) {
+            testStack.push((i + 1) * 4);
         }
+    }
+
+    @AfterAll
+    public static void clear() {
+        testStack.clear();
     }
 
     @Test
     @DisplayName("A stack is empty on construction")
-    void isEmptyOnConstruction() {
-        TqsStack stackOnConstr = new TqsStack();
-        assertTrue(stackOnConstr.isEmpty());
+    public void testInitEmpty() {
+        assertTrue(new TqsStack<Integer>().isEmpty(), "Stack should be empty on construction, and it's not");
+    }
+
+    @Test
+    @DisplayName("A stack is not full on construction")
+    public void testInitFull() {
+        assertFalse(new TqsStack<Integer>().isFull(), "Stack should not be full on construction, and it is");
     }
 
     @Test
     @DisplayName("A stack has size 0 on construction")
-    void sizeZeroOnConstruction() {
-        TqsStack stackOnConstr = new TqsStack();
-        assertEquals(0, stackOnConstr.size());
+    public void testInitSize() {
+        assertEquals(0, new TqsStack<Integer>().size(), "Stack should have size 0 on construction, and it doesn't");
+    }
+
+    @Test
+    @DisplayName("A stack is full when it has achieve it's bounded space")
+    public void testBoundedFull() {
+        TqsStack<Integer> boundedStack = new TqsStack<>(100);
+
+        this.pushN(boundedStack, 100);
+        assertTrue(boundedStack.isFull(), "Stack should be full, and it's not");
     }
 
     @Test
     @DisplayName("After n pushes to an empty stack, n > 0, the stack is not empty and its size is n")
-    void nPushes() {
-        TqsStack newStack = new TqsStack();
-        for(int i=0; i < 4; i++) {
-            newStack.push(i*3);
-        }
-        assertFalse(newStack.isEmpty());
-        assertEquals(4, newStack.size());
+    public void testPushSize() {
+        TqsStack<Integer> newStack = new TqsStack<>();
+        this.pushN(newStack, 4);
+        assertFalse(newStack.isEmpty(), "Stack shouldn't be empty");
+        assertEquals(4, newStack.size(), "Stack size is not 4, the expected size");
     }
 
     @Test
     @DisplayName("If one pushes x then pops, the value popped is x")
-    void pushThenPop() {
-        stack.push(10);
-        assertEquals(10, stack.pop());
+    public void testPushThenPop() {
+        testStack.push(10);
+        assertEquals(10, testStack.pop(), "The popped value is not the expected one");
     }
 
     @Test
     @DisplayName("If one pushes x then peeks, the value returned is x, but the size stays the same")
-    void pushThenPeeks() {
-        stack.push(10);
-        int originalSize = stack.size();
-        assertEquals(10, stack.peek());
-        assertEquals(originalSize, stack.size());
+    public void testPushThenPeek() {
+        testStack.push(10);
+        int originalSize = testStack.size();
+        assertEquals(10, testStack.peek(), "The peeked value is not the expected one");
+        assertEquals(originalSize, testStack.size(), "The peek operation should not change the size, but it did");
     }
 
     @Test
     @DisplayName("If the size is n, then after n pops, the stack is empty and has a size 0")
-    void nPops() {
-        int stackSize = stack.size();
-        for (int i = 0; i < stackSize; i++) {
-            stack.pop();
-        }
-        assertTrue(stack.isEmpty());
+    public void testPushNPopN() {
+        testStack.clear();
+        assertTrue(testStack.isEmpty(), "After clear operation, stack should be empty, and it's not");
+        this.popN(testStack, testStack.size());
+        assertTrue(testStack.isEmpty(), "After the multiple pop operations, stack should be empty, and it's not");
     }
 
     @Test
     @DisplayName("Popping from an empty stack does throw a NoSuchElementException")
-    void popFromEmptyStack() {
+    public void testPopEmptyStack() {
+        testStack.clear();
+        assertTrue(testStack.isEmpty(), "After clear operation, stack should be empty, and it's not");
         assertThrows(NoSuchElementException.class, () -> {
-            TqsStack newStack = new TqsStack();
-            newStack.pop();
+            testStack.pop();
         });
     }
 
     @Test
     @DisplayName("Peeking into an empty stack does throw a NoSuchElementException")
-    void peekFromEmptyStack() {
+    public void testPeekEmptyStack() {
+        testStack.clear();
+        assertTrue(testStack.isEmpty(), "After clear operation, stack should be empty, and it's not");
         assertThrows(NoSuchElementException.class, () -> {
-            TqsStack newStack = new TqsStack();
-            newStack.peek();
+            testStack.peek();
         });
+    }
+
+    @Test
+    @DisplayName("For bounded stacks only, pushing onto a full stack does throw an IllegalStateException")
+    public void testPushFullBoundedStack() {
+        TqsStack<Integer> boundedStack = new TqsStack<>(100);
+
+        this.pushN(boundedStack, 100);
+        assertThrows(IllegalStateException.class, () -> {
+            boundedStack.push(1);
+        });
+    }
+
+    private void pushN(TqsStack<Integer> stack, int n) {
+        for (int i = 0; i < n; i++) {
+            stack.push((i + 1) * 5);
+        }
+    }
+
+    private void popN(TqsStack<Integer> stack, int n) {
+        for (int i = 0; i < n; i++) {
+            stack.pop();
+        }
     }
 
 
